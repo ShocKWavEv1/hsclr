e569b5f0dba9f4aee3753e2117e9fef143f36e1e
ShocKWavEv1 <rodd.sal23@gmail.com>
2023-10-28 14:00:35 -0600

    fic custom cursor

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,16 +1,17 @@
 import Footer from "@/components/layout/footer/footer";
 import { customCursor } from "@/constants/constants";
 import { Box } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import HeroServices from "@/components/servicesView/hero/hero";
 import { useIsTouchDevice } from "@studio-freight/hamo";
 
 const Contact: React.FC = () => {
+  const videoRef = useRef(null);
   const isTouchableDevice = useIsTouchDevice();
 
   useEffect(() => {
     window.scroll({ top: 0 });
-    isTouchableDevice === false ? customCursor() : {};
+    isTouchableDevice === false ? customCursor(videoRef) : {};
   }, [isTouchableDevice]);
 
   return (
--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -3,14 +3,15 @@ import HeroTeam from "@/components/teamView/hero/hero";
 import { customCursor } from "@/constants/constants";
 import { Box } from "@chakra-ui/react";
 import { useIsTouchDevice } from "@studio-freight/hamo";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const Team: React.FC = () => {
+  const videoRef = useRef(null);
   const isTouchableDevice = useIsTouchDevice();
 
   useEffect(() => {
     window.scroll({ top: 0 });
-    isTouchableDevice === false ? customCursor() : {};
+    isTouchableDevice === false ? customCursor(videoRef) : {};
   }, [isTouchableDevice]);
 
   return (
